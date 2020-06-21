2d895231126ef8db929e99add8ecd99532e984d3
fazlay <rfazlay21@gmail.com>
2020-06-22 00:44:13 +0600

    Updates

---
+++ precache-manifest.1a9652a50d0c172c11dab6fa6e765057.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb202cbb8a486cb04bd966d7849a42f",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5507c9e93bba48b37ec5",
+    "url": "/RoboFriends/static/css/main.fe2f3eaf.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/RoboFriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5507c9e93bba48b37ec5",
+    "url": "/RoboFriends/static/js/main.0e9ec4d8.chunk.js"
+  },
+  {
+    "revision": "671d1b49487ff131bdad",
+    "url": "/RoboFriends/static/js/runtime-main.16920bcc.js"
+  }
+]);
